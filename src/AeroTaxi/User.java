8e66fc1f474cb8d7efbe38f23771aac6876bad2d
facundoloscos <56739064+facundoloscos@users.noreply.github.com>
2020-06-01 15:22:25 -0300

    first commit

--- src/AeroTaxi/User.java
+++ src/AeroTaxi/User.java
@@ -2,7 +2,19 @@ package AeroTaxi;
 
 public class User {
 
-    private String name,lastName,DNI;
+    private String name;
+    private String lastName;
+    private String DNI;
     private int age;
 
+    public User(String name, String lastName, String DNI, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.DNI = DNI;
+        this.age = age;
+    }
+
+    public User() {
+
+    }
 }
