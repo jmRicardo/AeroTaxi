4a344c0987a79850374c47b8695221c6f72b9def
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2020-06-01 17:37:21 -0300

    trabajando en las ventanas

--- src/AeroTaxi/NewUser.java
+++ src/AeroTaxi/NewUser.java
@@ -2,8 +2,11 @@ package AeroTaxi;
 
 import javax.swing.*;
 import javax.swing.plaf.metal.MetalLookAndFeel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class NewUser extends JFrame{
     private JTextField nameField;
@@ -13,6 +16,8 @@ public class NewUser extends JFrame{
     private JButton okButton;
     private JPanel root;
     private JButton backButton;
+    private JLabel isValidLabel;
+    private JLabel exampleLabel;
 
     public NewUser(){
 
@@ -31,5 +36,38 @@ public class NewUser extends JFrame{
         okButton.addActionListener(e -> {
 
         });
+
+        KeyAdapter checker = new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+                checkStatus();
+            }
+        };
+
+
+        dniField.addKeyListener(checker);
+        nameField.addKeyListener(checker);
+        ageField.addKeyListener(checker);
+        lastField.addKeyListener(checker);
+    }
+
+    private void checkStatus(){
+
+        boolean dniBool = AeroTaxi.checkDni(dniField.getText());
+        boolean ageBool = AeroTaxi.checkAge(ageField.getText());
+        boolean nameBool = !nameField.getText().isEmpty();
+        boolean lastBool = !lastField.getText().isEmpty();
+
+        if (dniBool && ageBool && nameBool && lastBool){
+            isValidLabel.setForeground(Color.green);
+            isValidLabel.setText("formato valido!");
+            isValidLabel.setVisible(false);
+            okButton.setEnabled(true);
+        }else{
+            isValidLabel.setForeground(Color.red);
+            isValidLabel.setText("formulario incompleto!");
+            okButton.setEnabled(false);
+        }
     }
 }
